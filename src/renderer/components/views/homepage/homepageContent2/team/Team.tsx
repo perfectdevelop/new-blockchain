9d89d055f065a4c1ff7b9869ba4fd51bd09270cd
pheonix2021 <pheonix20210101@gmail.com>
2021-10-23 21:37:43 +0300

    10-23-09-37

--- src/renderer/components/views/homepage/homepageContent2/team/Team.tsx
+++ src/renderer/components/views/homepage/homepageContent2/team/Team.tsx
@@ -18,7 +18,6 @@ export default function Team(): JSX.Element {
         <TeamMember ape={MonoApe} name="Tony" role="Art & Design" description="The founder, also in charge of the community building and marketing. A true believer of the community-based decentralization approach and believe in cryptocurrency since 2017."/>
         <TeamMember ape={OboApe} name="Danny" role="Project Management" description="Experienced Developer in blockchain and game development, majored in computer science and has knowledge on BSC, ETH and SOL projects."/>
         <TeamMember ape={ConorApe} name="ConorHolds" role="Developer" description="Specialized in digital graphic design, worked on multiple NFT projects and is great on digital arts."/>
-        
       </div>
     </div>
   );
